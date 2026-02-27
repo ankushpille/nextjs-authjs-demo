a0f2d6b8f0cfefe36a765bfdab701c1bd70e3aec
ankushpille <ankush.pille1@gmail.com>
2026-02-27 22:08:36 +0530

    refactor

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,9 @@ import { withAuth } from "next-auth/middleware";
 
 // Protect routes using Auth.js middleware wrapper.
 export default withAuth({
+  pages: {
+    signIn: "/"
+  },
   callbacks: {
     authorized: ({ token }) => !!token
   }
